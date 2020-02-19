529ddfaea89d1ef4fb9f8a2ca63a3a04b874feca
Umair Anser <umair.anser115@gmail.com>
2020-02-19 19:17:03 +0500

    redux added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import Main from './Pages/Main/Main'
+import { Provider } from 'react-redux';
 
 class App extends Component {
   render(){
     return (
+      <Provider store={store}>
      <Main/>
+     </Provider>
     );
 
   }
